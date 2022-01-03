409775d8af21ef9cbe6f0c2fb47da9234d1d3650
frenkieLee <frenkie.lee@gstech.io>
2022-01-03 18:04:28 +0800

    操作鏡頭和顯示 fps

--- index.js
+++ index.js
@@ -1,46 +1,86 @@
+import { OrbitControls } from "../lib/OrbitControls.js";
+
+let renderer, scene, camera;
+let cameraControl, stats;
+
 // 生成苦力怕並加到場景
-function createCreeper(scene) {
+function createCreeper() {
   const creeperObj = new Creeper();
   scene.add(creeperObj.creeper);
 }
 
-function init() {
-  let scene = new THREE.Scene();
-  let renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x000000, 1.0);
-  renderer.shadowMap.enable = true;
-  document.body.appendChild(renderer.domElement);
+function initStats() {
+  const stats = new Stats();
+  stats.setMode(0);
+  document.getElementById("stats").appendChild(stats.domElement);
+  return stats;
+}
 
-  // 相機位置
-  let camera = new THREE.PerspectiveCamera(
-    45,
+// 畫面初始化
+function init() {
+  scene = new THREE.Scene();
+
+  // 相機設定
+  camera = new THREE.PerspectiveCamera(
+    60,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
-  camera.position.set(50, 50, 50); // 相機位置
-  camera.lookAt(scene.position); // 相機焦點
+  camera.position.set(30, 30, 30);
+  camera.lookAt(scene.position);
 
-  // 光源
+  // 三軸座標輔助
+  let axes = new THREE.AxesHelper(20);
+  scene.add(axes);
 
-  let pointLight = new THREE.PointLight(0xffffff);
-  pointLight.position.set(-20, 40, 20);
-  scene.add(pointLight);
+  stats = initStats();
 
-  // 產生苦力怕
-  createCreeper(scene);
+  // 渲染器設定
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // 建立 OrbitControls
+  cameraControl = new OrbitControls(camera, renderer.domElement);
+  cameraControl.enableDamping = true; // 啟用阻尼效果
+  cameraControl.dampingFactor = 0.25; // 阻尼系數
+  // cameraControl.autoRotate = true // 啟用自動旋轉
 
   // 簡單的地板
   const planeGeometry = new THREE.PlaneGeometry(60, 60);
   const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
   let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.rotation.x = -0.5 * Math.PI; // 使平面與 y 軸垂直，並讓正面朝上
+  plane.rotation.x = -0.5 * Math.PI;
   plane.position.set(0, -7, 0);
   scene.add(plane);
-  let axes = new THREE.AxesHelper(20); // 參數為座標軸長度
-  scene.add(axes);
+
+  // 產生苦力怕物件並加到場景
+  createCreeper();
+
+  // 簡單的 spotlight 照亮物體
+  let spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(-10, 40, 30);
+  scene.add(spotLight);
+  // let spotHelper = new THREE.SpotLightHelper(spotLight)
+  // scene.add(spotHelper)
+
+  // 將渲染出來的畫面放到網頁上的 DOM
+  document.body.appendChild(renderer.domElement);
+}
+
+function render() {
+  stats.update();
+  cameraControl.update();
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
 
 init();
+render();
+
+// 自動隨著畫面縮放
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
