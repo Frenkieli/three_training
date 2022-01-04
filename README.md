4c397659e811f6167a0ccabf488fdc0ec4587446
frenkieLee <frenkie.lee@gstech.io>
2022-01-04 10:22:43 +0800

    環繞苦力帕光點

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # three_training
 練習three.js的練習檔
+
+
+目前完成 https://ithelp.ithome.com.tw/articles/10204739
--- index.js
+++ index.js
@@ -2,14 +2,9 @@ import { OrbitControls } from "../lib/OrbitControls.js";
 
 let renderer, scene, camera;
 let cameraControl, stats, gui;
-let ambientLight, pointLight, spotLight, directionalLight;
-
-let datGUIControls = new (function () {
-  this.AmbientLight = true;
-  this.PointLight = false;
-  this.Spotlight = false;
-  this.DirectionalLight = false;
-})();
+let ambientLight, pointLight, spotLight, directionalLight, sphereLightMesh;
+let rotateAngle = 0;
+let invert = 1;
 
 // 生成苦力怕並加到場景
 function createCreeper() {
@@ -24,11 +19,27 @@ function initStats() {
   return stats;
 }
 
+// 點光源繞 Y 軸旋轉動畫
+function pointLightAnimation() {
+  if (rotateAngle > 2 * Math.PI) {
+    rotateAngle = 0; // 超過 360 度後歸零
+  } else {
+    rotateAngle += 0.03; // 遞增角度
+  }
+
+  // 光源延橢圓軌道繞 Y 軸旋轉
+  sphereLightMesh.position.x = 8 * Math.cos(rotateAngle);
+  sphereLightMesh.position.z = 4 * Math.sin(rotateAngle);
+
+  // 點光源位置與球體同步
+  pointLight.position.copy(sphereLightMesh.position);
+}
+
 // 畫面初始化
 function init() {
   // 建立場景
   scene = new THREE.Scene();
-  
+
   // 顯示動畫影格
   stats = initStats();
   // 三軸座標輔助
@@ -39,9 +50,8 @@ function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   // 渲染陰影
-  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.enabled = true; // 設定需渲染陰影效果
   renderer.shadowMap.type = 2; // THREE.PCFSoftShadowMap
-
 
   // 相機設定
   camera = new THREE.PerspectiveCamera(
@@ -65,12 +75,74 @@ function init() {
   let plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.rotation.x = -0.5 * Math.PI;
   plane.position.set(0, -7, 0);
-  plane.receiveShadow = true; // 顯示陰影
+  plane.receiveShadow = true; // 接受陰影
   scene.add(plane);
 
   // 產生苦力怕物件並加到場景
   createCreeper();
 
+  // 設置環境光與聚光燈
+  let ambientLight = new THREE.AmbientLight(0x404040);
+  scene.add(ambientLight);
+
+  // 設置聚光燈幫忙照亮物體
+  let spotLight = new THREE.SpotLight(0xf0f0f0);
+  spotLight.position.set(-10, 30, 20);
+  // spotLight.castShadow = true;
+  let spotLightHelper = new THREE.SpotLightHelper(spotLight);
+  scene.add(spotLightHelper);
+  scene.add(spotLight);
+
+  // 移動點光源
+
+  pointLight = new THREE.PointLight(0xccffcc, 1, 100); // 顏色, 強度, 距離
+  pointLight.castShadow = true; // 這個光源會有陰影
+  // let pointLightHelper = new THREE.PointLightHelper(pointLight);
+  // scene.add(pointLightHelper);
+  scene.add(pointLight);
+
+  // 小球體模擬點光源實體
+  const sphereLightGeo = new THREE.SphereGeometry(0.3);
+  const sphereLightMat = new THREE.MeshBasicMaterial({ color: 0xccffcc });
+  sphereLightMesh = new THREE.Mesh(sphereLightGeo, sphereLightMat);
+  sphereLightMesh.castShadow = true;
+  sphereLightMesh.position.y = 16;
+  scene.add(sphereLightMesh);
+
+
+
+  // testLightTool(); // 光源測試
+  // 將渲染出來的畫面放到網頁上的 DOM
+  document.body.appendChild(renderer.domElement);
+}
+
+function render() {
+  stats.update();
+  cameraControl.update();
+  pointLightAnimation(); // 更新光點動畫
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+init();
+render();
+
+// 自動隨著畫面縮放
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function testLightTool() {
+  // 光源測試
+
+  let datGUIControls = new (function () {
+    this.AmbientLight = true;
+    this.PointLight = false;
+    this.Spotlight = false;
+    this.DirectionalLight = false;
+  })();
   // 設置環境光 AmbientLight
   ambientLight = new THREE.AmbientLight(0xeeff00);
   scene.add(ambientLight);
@@ -124,24 +196,4 @@ function init() {
     directionalLight.visible = e;
     directionalLightHelper.visible = e;
   });
-
-  // 將渲染出來的畫面放到網頁上的 DOM
-  document.body.appendChild(renderer.domElement);
 }
-
-function render() {
-  stats.update();
-  cameraControl.update();
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
-
-init();
-render();
-
-// 自動隨著畫面縮放
-window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
